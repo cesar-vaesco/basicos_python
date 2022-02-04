fa392b754555c91cfe2940e1da76219fe62e06ba
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-02-04 14:45:36 -0600

    variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+# Variables
+dato_none = None
+print(dato_none)
+
+nombre = "César"
+print('Nombre: ' + nombre)
+
+x = 100
+libro = "La Dama Encantada"
+print(x)
+print(libro)
+
+valor, apodo = 100, "Chicharo"
+print("Valor: " + str(valor))
+print('Apodo: ' + apodo)
+
+# Constantes
+PI = 3.1416
+print(PI)
+
+
+nombre = "Vero"
+print(nombre)
