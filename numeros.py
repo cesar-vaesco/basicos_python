12991fef0250ffeabc82eedf55da5268da1b5cd7
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-02-04 18:00:20 -0600

    Números

---
+++ numeros.py
@@ -0,0 +1,21 @@
+
+edad = input("Inserta tu edad: ")
+print(type(edad))
+nueva_edad = int(edad) + 5
+print(nueva_edad)
+print(type(nueva_edad))
+#print(type(3))
+#print(type(3.5))
+#
+#print(1 + 1)
+#print(1 + 1.5)
+#print(1 - 1)
+#print(1 * 1)
+#print(5 / 6)
+#print(3 ** 2)
+#print(3 / 2)
+#print(3 // 2)
+#print(3 % 2)
+#print(10 % 3)
+
+
