526b85e07b24c658c68f303ed689e60e71554323
cesar-vaesco <cesarvargasescorcia@gmail.com>
2022-02-09 20:08:36 -0600

    Editando archivo de manejo de errores

--- archivos/manejo_errores.py
+++ archivos/manejo_errores.py
@@ -1,19 +1,28 @@
-import sys
-
-file_name = 'recipes.txt'
+#import sys
+#
+#file_name = 'recipes.txt'
+#
+#try:
+#    mi_archivo = open('recipes.txt', 'x')
+#    mi_archivo.write('Meatballs\n')
+#    mi_archivo.close()
+#except FileExistsError as err:
+#    print ('folder {0} already exists'.format(file_name))
+#    print(err)
+#    sys.exit(1)
+#except:
+#    print('no se puede escribir en el archivo {0}'.format(file_name))
+#    sys.exit(1)
+#else:
+#    print("Escribir en {0}".format(file_name))
+#finally:
+#    print("Ejecucion completa")
 
 try:
-    mi_archivo = open('recipes.txt', 'x')
-    mi_archivo.write('Meatballs\n')
-    mi_archivo.close()
-except FileExistsError as err:
-    print ('folder {0} already exists'.format(file_name))
-    print(err)
-    sys.exit(1)
-except:
-    print('no se puede escribir en el archivo {0}'.format(file_name))
-    sys.exit(1)
-else:
-    print("Escribir en {0}".format(file_name))
+    print(10/0)
+except TypeError as e:
+    print("ERROR EN EL SISTEMA - TIPO ", e)
+except ZeroDivisionError as ex:
+    print("ERROR EN EL SISTEMA - DIVISION : ", ex)
 finally:
-    print("Ejecucion completa")
+    print("De todas formas llegamos hasta este punto del programa")
